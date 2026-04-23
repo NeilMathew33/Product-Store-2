47c7c54d91a8a54f3d420f41686dac1a0441dc58
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-22 18:55:27 -0700

    added signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes,  Route } from 'react-router-dom';
 import AddProduct from './Components/AddProduct';
 import ViewAllProducts from './Components/ViewAllProducts';
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
    <Routes>
     <Route path="/Add" element= {<AddProduct />} />
     <Route path="/ViewAll" element= {<ViewAllProducts />} />
-    <Route path="/" element= {<Login />} />
+    <Route path="/" element= {<Login />} />Z
+    <Route path="/SignUp" element= {<SignUp />} />Z
    </Routes>
    </BrowserRouter>
   );
